2804462c5d78d9049e70e8f852d0fb509cd77508
Yulong Tan <swifties@uw.edu>
2015-12-31 02:44:08 -0800

    refactor code into functions

--- dubblebooked/scripts/js.js
+++ dubblebooked/scripts/js.js
@@ -5,55 +5,54 @@
 "use strict";
 
 $('#court').hide();
-$('#court').addEventListener('change', showElement, false);
+$('#court').addEventListener('change', changeRequest, false);
 $('#room').hide();
-$('#room').addEventListener('change', showElement, false);
+$('#room').addEventListener('change', changeRequest, false);
 $('#date').hide();
-$('#date').addEventListener('change', showElement, false);
+$('#date').addEventListener('change', changeRequest, false);
 $('#time').hide();
-$('#time').addEventListener('change', showElement, false);
+$('#time').addEventListener('change', changeRequest, false);
 $('#jis').hide();
-$('#jis').addEventListener('change', showElement, false);
+$('#jis').addEventListener('change', changeRequest, false);
+
+var request = {
+    court : null,
+    courtroom : null,
+    date : null,
+    time : null
+};
+
+var count = 1;
 
 
 window.onload = function () {
-    // alert("hello");
-    $.post("../..", function (data) {
-        //console.log("Data Loaded: " + data );
-        //console.log(data[0]);
-
-        //for (var i = 0; i < data.length; i++) {
-        //    console.log(data[i]);
-        //}
-        //console.log(i + 1);
-        var parent = document.getElementById("court");
-        for (var i = 0; i < data.length; i++) {
-            var node = document.createElement("OPTION");
-            node.innerHTML = data[i];
-            parent.appendChild(node);
-        }
-        $('#court').show();
-    });
-
-    $.post("../..", {court: "John"})
-        .done(function (data) {
-            alert("Data Loaded: " + data);
-        });
-
-    var courtSelect = $('#court');
-    var roomSelect = document.getElementById("room");
-
-
-    function showElement() {
-        if (this.value == "court") {
-            $('#room').show();
-        } else if (this.value == "room") {
-            $('#date').show();
-        } else if (this.value == "date") {
-            $('#time').show();
-        } else {
-            $('#jis').show();
-        }
-    }
+    requestAndFill("court");
 };
+
+function changeRequest() {
+    request[this.getAttribute("id")] = this.value;
+    requestAndFill(this.getAttribute("id"));
+    if (count <= 3) {
+        if (count == 1) {
+            $('#room').show();
+        } else if (count == 2) {
+            $('#date').show();
+        } else if (count == 3) {
+            $('#time').show();
+        }
+        count++;
+    }
+}
+
+function requestAndFill(s) {
+    $.post("../..", request)
+        .done(function (data) {
+            var parent = document.getElementById(s);
+            for (var i = 0; i < data.length; i++) {
+                var node = document.createElement("OPTION");
+                node.innerHTML = data[i];
+                parent.appendChild(node);
+            }
+        });
+}
 
