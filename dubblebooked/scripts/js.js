19dc7199e9a296762f9899d03b1b75ab40c83dc6
Yulong Tan <swifties@uw.edu>
2015-12-31 04:22:31 -0800

    updated methods to change based on what is clicked

--- dubblebooked/scripts/js.js
+++ dubblebooked/scripts/js.js
@@ -1,58 +1,106 @@
 /**
- * Created by Will on 12/31/2015.
+ * Created by Will and Yulong on 12/31/2015.
  */
+(function () {
+    "use strict";
 
-"use strict";
+    $('#court').hide();
+    document.getElementById("court").addEventListener('change', changeRequestCourt, false);
+    $('#room').hide();
+    document.getElementById("room").addEventListener('change', changeRequestRoom, false);
+    $('#date').hide();
+    document.getElementById("date").addEventListener('change', changeRequestDate, false);
+    $('#time').hide();
+    document.getElementById("time").addEventListener('change', changeRequestTime, false);
+    $('#jis').hide();
+//document.getElementById("jis").addEventListener('change', changeRequest, false);
 
-$('#court').hide();
-$('#court').addEventListener('change', changeRequest, false);
-$('#room').hide();
-$('#room').addEventListener('change', changeRequest, false);
-$('#date').hide();
-$('#date').addEventListener('change', changeRequest, false);
-$('#time').hide();
-$('#time').addEventListener('change', changeRequest, false);
-$('#jis').hide();
-$('#jis').addEventListener('change', changeRequest, false);
+    var request = {
+        court: null,
+        room: null,
+        date: null,
+        time: null
+    };
 
-var request = {
-    court : null,
-    courtroom : null,
-    date : null,
-    time : null
-};
-
-var count = 1;
+    var count = 1;
 
 
-window.onload = function () {
-    requestAndFill("court");
-};
+    window.onload = function () {
+        requestAndFill("court");
+    };
 
-function changeRequest() {
-    request[this.getAttribute("id")] = this.value;
-    requestAndFill(this.getAttribute("id"));
-    if (count <= 3) {
-        if (count == 1) {
-            $('#room').show();
-        } else if (count == 2) {
-            $('#date').show();
-        } else if (count == 3) {
-            $('#time').show();
-        }
-        count++;
+// If court is selected
+    function changeRequestCourt() {
+        request[this.getAttribute("id")] = this.value;
+        $('#room').empty();
+        $('#room').append("<option disabled=\"disabled\" selected=\"selected\">Room</option>");
+        $('#date').empty();
+        $('#date').append("<option disabled=\"disabled\" selected=\"selected\">Date</option>");
+        $('#time').empty();
+        $('#time').append("<option disabled=\"disabled\" selected=\"selected\">Time</option>");
+        request["room"] = "";
+        request["date"] = "";
+        request["time"] = "";
+        requestAndFill("room");
     }
-}
 
-function requestAndFill(s) {
-    $.post("../..", request)
-        .done(function (data) {
-            var parent = document.getElementById(s);
-            for (var i = 0; i < data.length; i++) {
-                var node = document.createElement("OPTION");
-                node.innerHTML = data[i];
-                parent.appendChild(node);
+// Room is clicked
+    function changeRequestRoom() {
+        request[this.getAttribute("id")] = this.value;
+        $('#date').empty();
+        $('#date').append("<option disabled=\"disabled\" selected=\"selected\">Date</option>");
+        $('#time').empty();
+        $('#time').append("<option disabled=\"disabled\" selected=\"selected\">Time</option>");
+        request["date"] = "";
+        request["time"] = "";
+        requestAndFill("date");
+    }
+
+    function changeRequestDate() {
+        request[this.getAttribute("id")] = this.value;
+        $('#time').empty();
+        $('#time').append("<option disabled=\"disabled\" selected=\"selected\">Time</option>");
+        request["time"] = "";
+        requestAndFill("time");
+
+    }
+
+    function changeRequestTime() {
+        requestAndFill(this.getAttribute("id"));
+    }
+
+    function requestAndFill(s) {
+        console.log(s);
+        $.post("../..", request)
+            .done(function (data) {
+                var parent = document.getElementById(s);
+                for (var i = 0; i < data.length; i++) {
+                    var node = document.createElement("OPTION");
+                    if (s == "time")
+                        node.innerHTML = parseTime(data[i]);
+                    else
+                        node.innerHTML = data[i];
+                    parent.appendChild(node);
+
+                }
+                $("#" + s).show();
+                console.log(data);
+            });
+    }
+
+    function parseTime(t) {
+        if (parseInt(t.substring(0, 2)) > 12) {
+            var time = parseInt(t.substring(0, 2)) - 12;
+            if (time < 10) {
+                time = "0" + time;
             }
-        });
-}
+            return time + t.substring(2) + " PM"
+        } else if (parseInt(t.substring(0, 2)) == 12) {
+            return t + " PM";
+        } else {
+            return t + " AM";
+        }
+
+    }
+})();
 
